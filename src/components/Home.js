86e662b1a7a8994c6fe905fe4a1b3b1e7bf0f5b1
vijayskk <vijayskk007@gmail.com>
2022-05-04 10:24:52 +0530

    Recommendation image fixed

--- src/components/Home.js
+++ src/components/Home.js
@@ -45,21 +45,21 @@ function Home() {
                     </div>
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                            <img src={data[0].image} class="w-80 mx-auto d-block img-fluid img-thumbnail" alt="..." />
+                            <img src={data[0].image} class="w-80 mx-auto darkmode-ignore image d-block img-fluid img-thumbnail" alt="..." />
                                 <div class="carousel-caption d-none d-md-block">
                                     <h5>First slide label</h5>
                                     <p>Some representative placeholder content for the first slide.</p>
                                 </div>
                         </div>
                         <div class="carousel-item">
-                            <img src={data[0].image}  class="w-80 mx-auto d-block img-fluid img-thumbnail" alt="..." />
+                            <img src={data[0].image}  class="w-80 mx-auto darkmode-ignore image d-block img-fluid img-thumbnail" alt="..." />
                                 <div class="carousel-caption d-none d-md-block">
                                     <h5>Second slide label</h5>
                                     <p>Some representative placeholder content for the second slide.</p>
                                 </div>
                         </div>
                         <div class="carousel-item">
-                            <img src={data[0].image}  class="w-80 mx-auto d-block img-fluid img-thumbnail" alt="..." />
+                            <img src={data[0].image}  class="w-80 mx-auto darkmode-ignore image d-block img-fluid img-thumbnail" alt="..." />
                                 <div class="carousel-caption d-none d-md-block">
                                     <h5>Third slide label</h5>
                                     <p>Some representative placeholder content for the third slide.</p>
