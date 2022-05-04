43ba7be71a36fa36d4b0017f33fd1c738a7c715a
vijayskk <vijayskk007@gmail.com>
2022-05-04 10:28:32 +0530

    Darkmode disabled

--- src/App.js
+++ src/App.js
@@ -9,22 +9,22 @@ import { CartContextProvider } from './contexts/CartContext';
 
 
 function App() {
-  const options = {
-    bottom: '64px', // default: '32px'
-    right: '32px', // default: '32px'
-    left: 'unset', // default: 'unset'
-    time: '0.5s', // default: '0.3s'
-    mixColor: '#fff', // default: '#fff'
-    backgroundColor: '#fff',  // default: '#fff'
-    buttonColorDark: '#100f2c',  // default: '#100f2c'
-    buttonColorLight: '#fff', // default: '#fff'
-    saveInCookies: false, // default: true,
-    label: '🌓', // default: ''
-    autoMatchOsTheme: true // default: true
-  }
+  // const options = {
+  //   bottom: '64px', // default: '32px'
+  //   right: '32px', // default: '32px'
+  //   left: 'unset', // default: 'unset'
+  //   time: '0.5s', // default: '0.3s'
+  //   mixColor: '#fff', // default: '#fff'
+  //   backgroundColor: '#fff',  // default: '#fff'
+  //   buttonColorDark: '#100f2c',  // default: '#100f2c'
+  //   buttonColorLight: '#fff', // default: '#fff'
+  //   saveInCookies: false, // default: true,
+  //   label: '🌓', // default: ''
+  //   autoMatchOsTheme: true // default: true
+  // }
   
-  const darkmode = new Darkmode(options);
-  darkmode.showWidget();
+  // const darkmode = new Darkmode(options);
+  // darkmode.showWidget();
   return (
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
